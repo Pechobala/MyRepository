ec5098cb6b06b7b33139792673efbaae1f227714
Pechobala <s.uso95@hotmail.es>
2026-04-23 14:24:45 +0000

    Clean code

--- test_login.py
+++ test_login.py
@@ -1,19 +1,16 @@
 from playwright.sync_api import Page
+import pytest
 
 @pytest.mark.parametrize("myname, myemail, mycompany", [
     ("Juan", "juan@email.com", "Companyex1"),
     ("Ana", "ana@email.com", "Companyex2"),
     ("Pedro", "pedro@email.com", "Companyex3"),
 ])
-def test_loggin(page: Page, myname, myemail, mycompany):
+def test_login(page: Page, myname, myemail, mycompany):
     page.goto("https://www.assaia.com/#")
     
-    #page.locator(".btn_label", has_text="Get in touch").click()
-    #page.locator(".btn_primary", has_text="Get in touch").click()
     page.get_by_role("main").get_by_role("link", name="Get in touch").click()
-    assert page.get_by_role("heading", name="Send us a message").inner_text() == "Send us a message"
 
-    #name, Email Address, Company
     page.locator("[name='name']").fill(myname) 
     page.locator("[name='email']").fill(myemail)
     page.locator("[name='Company']").fill(mycompany)  
